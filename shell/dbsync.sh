83287a137b3e7647c0dc0c77661f176a3363ce90
maojiangyun <maojiangyun@bilibili.com>
2022-08-22 11:45:47 +0800

    修改完善db sync脚本

--- shell/dbsync.sh
+++ shell/dbsync.sh
@@ -34,11 +34,12 @@ function _sync_blackhole_table() {
     table_name=$1
     blackhole_prod_db_table="${cache_dir}/${table_name}.prod.sql.${backtime}"
     /usr/bin/mysqldump -h${PROD_HOST} -P${PROD_PORT} -u${PROD_USER} \
-        -p${PROD_PSWD} ${PROD_DB} ${table_name} --skip-lock-tables \
+        -p${PROD_PSWD} ${PROD_DB} ${table_name} --skip-lock-tables --single-transaction \
         > ${blackhole_prod_db_table}
     /usr/bin/mysql -h${blackhole_pre_host} -P${blackhole_pre_port} -u${blackhole_pre_user} \
-        -p${blackhole_pre_pswd} ${blackhole_pre_db} ${table_name} \
+        -p${blackhole_pre_pswd} ${blackhole_pre_db} \
         < ${blackhole_prod_db_table}
+    rm -f ${blackhole_prod_db_table}
 }
 
 function sync_blackhole_db() {
@@ -56,11 +57,12 @@ function _sync_polaris_table() {
     table_name=$1
     polaris_prod_db_table="${cache_dir}/${table_name}.prod.sql.${backtime}"
     /usr/bin/mysqldump -h${PROD_HOST} -P${PROD_PORT} -u${PROD_USER} \
-        -p${PROD_PSWD} ${PROD_DB} ${table_name} --skip-lock-tables \
+        -p${PROD_PSWD} ${PROD_DB} ${table_name} --skip-lock-tables --single-transaction \
         > ${polaris_prod_db_table}
     /usr/bin/mysql -h${polaris_pre_host} -P${polaris_pre_port} -u${polaris_pre_user} \
-        -p${polaris_pre_pswd} ${polaris_pre_db} ${table_name} \
+        -p${polaris_pre_pswd} ${polaris_pre_db} \
         < ${polaris_prod_db_table}
+    rm -f ${polaris_prod_db_table}
 }
 
 function sync_polaris_db() {
@@ -73,14 +75,13 @@ function sync_polaris_db() {
     _sync_polaris_table "polaris_pool_type"
     _sync_polaris_table "polaris_pool"
     
-    echo "UPDATE polaris_pool SET reserve=0;" > "${cache_dir}/update.sql"
     /usr/bin/mysql -h${polaris_pre_host} -P${polaris_pre_port} -u${polaris_pre_user} \
-        -p${polaris_pre_pswd} ${polaris_pre_db} polaris_pool \
-        < "${cache_dir}/update.sql"
+        -p${polaris_pre_pswd} ${polaris_pre_db} -e "
+        UPDATE polaris_pool SET reserve=0;
+        "
     
     /data/service/hadoop/bin/hadoop fs -put ${polaris_pre_db_bk} ${HDFS_PREFIX}
 }
 
 sync_blackhole_db
 sync_polaris_db
-
